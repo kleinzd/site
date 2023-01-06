cfee54afe05f7c2cf30820b34520dd2f55dde7d9
kleinzd <kleinzmail@gmail.com>
2023-01-06 23:11:40 +0800

    boccher

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,11 @@ import NavbarVue from "./components/NavbarVue.vue";
 
 <template>
   <NavbarVue></NavbarVue>
-  <div class="bg-[#343434] w-screen min-h-[calc(100vh-70px-45px)] flex">
+  <div class="w-[calc(100wh-20px)] min-h-[calc(100vh-70px-45px-20px)] flex">
     <RouterView></RouterView>
   </div>
   <div
-    class="w-screen h-[45px] bg-[#343434] flex items-center justify-center text-center"
+    class="w-[calc(100wh-20px)] flex items-center justify-center text-center"
   >
     <p class="text-white font-semibold">&copy; Kleinz</p>
   </div>
--- src/components/NavbarVue.vue
+++ src/components/NavbarVue.vue
@@ -1,20 +1,22 @@
 <script setup lang="ts">
 import { RouterLink } from "vue-router";
-import img from "../assets/takina.png";
+import img from "../assets/nijika.jpg";
 </script>
 
 <template>
-  <div class="bg-[#292929] w-screen h-[70px] flex items-center px-5">
+  <div
+    class="bg-[#51E1ED] w-[calc(100wh-20px)] h-[calc(70px)] flex items-center px-3 rounded-3xl shadow-lg z-10"
+  >
     <ul class="flex flex-column gap-7 items-center">
       <li class="transition hover:translate-y-3">
         <RouterLink :to="{ name: 'Home' }"
           ><img :src="img" class="h-[55px] w-[55px] shadow-lg rounded-full"
         /></RouterLink>
       </li>
-      <li class="text-white font-semibold hover:text-teal-500 transition">
+      <li class="text-[#03203C] font-semibold hover:text-[#207398] transition">
         <RouterLink to="/">Home</RouterLink>
       </li>
-      <li class="text-white font-semibold hover:text-teal-500 transition">
+      <li class="text-[#03203C] font-semibold hover:text-[#207398] transition">
         <RouterLink to="/about">About</RouterLink>
       </li>
     </ul>
